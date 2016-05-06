9d9e82dbebdc51c2d1726fef1d8f4d733fd1f081
henrikrudstrom <hrudstrom@gmail.com>
2016-05-06 13:03:58 +0000

    tests: Activate recursive props tests
    PR-URL: https://github.com/qmlweb/qmlweb/pull/233
    Reviewed-By: Сковорода Никита Андреевич <chalkerx@gmail.com>

--- tests/failingTests.js
+++ tests/failingTests.js
@@ -8,10 +8,6 @@ window.failingTests = {
     parse: [
       'can parse a function assigned to a var property'
     ],
-    imports: [
-      'RecursiveInit',
-      'RecursiveInit2'
-    ],
     properties: [
       'alias to id with same name',
       'ChangedExpressionSignal',
