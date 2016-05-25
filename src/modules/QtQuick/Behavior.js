8eab2d320f0a8d70daa66d160f74addfe170cade
Stephen D'Angelo <stephen.m.dangelo@gmail.com>
2016-05-25 16:23:22 -0400

    WIP Initial test of using transitions for behaviors

--- src/modules/QtQuick/Behavior.js
+++ src/modules/QtQuick/Behavior.js
@@ -10,12 +10,35 @@ registerQmlType({
     this.$defaultProperty = "animation";
     createProperty("bool", this, "enabled", {initialValue: true});
 
+    var self = this;
+
+    function useTransition(animation) {
+        var transitionTarget;
+        if (meta.object.$on === "scale") {
+            transitionTarget = "transform";
+        } else
+        if (meta.object.$on === "opacity") {
+            transitionTarget = "opacity";
+        } else {
+            return false
+        }
+
+        self.$parent.dom.style.transition = transitionTarget + " " + animation.duration + "ms";
+        return true;
+    }
+
     this.animationChanged.connect(this, function(newVal) {
+        if (useTransition(newVal)) {
+            return;
+        }
         newVal.target = this.$parent;
         newVal.property = meta.object.$on;
         this.$parent.$properties[meta.object.$on].animation = newVal;
     });
     this.enabledChanged.connect(this, function(newVal) {
+        if (useTransition(this.animation)) {
+            return;
+        }
         this.$parent.$properties[meta.object.$on].animation = newVal ? this.animation : null;
     });
   }
