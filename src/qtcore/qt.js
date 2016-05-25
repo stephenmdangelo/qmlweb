5b9e8da99b7fae9e8e5b399d058c632e5da3c09d
Stephen D'Angelo <stephen.m.dangelo@gmail.com>
2016-05-25 16:22:33 -0400

    qt: Added Item alignment enumerator

--- src/qtcore/qt.js
+++ src/qtcore/qt.js
@@ -380,3 +380,17 @@ global.Qt = {
   ScrollBarAlwaysOff: 1,
   ScrollBarAlwaysOn: 2
 }
+
+global.Item = {
+    // Alignment
+    TopLeft: 0,
+    Top: 1,
+    TopRight: 2,
+    Left: 3,
+    Center: 4,
+    Right: 5,
+    BottomLeft: 6,
+    Bottom: 7,
+    BottomRight: 8
+}
+
